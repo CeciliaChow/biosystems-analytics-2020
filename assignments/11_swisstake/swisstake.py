0af2040ba8ff0e0dad02e59adae6a5608afc7001
Cecilia Chow <ceciliachow@on-campus-10-138-64-202.vpn.arizona.edu>
2020-05-04 11:27:42 -0700

    swisstake.py

--- assignments/11_swisstake/swisstake.py
+++ assignments/11_swisstake/swisstake.py
@@ -4,7 +4,6 @@ Although I have a trouble passing the test, I feel like I have learnt a lot
 from coding this. I was hoping to get some credits form handing in still
 as I have been working really hard for this."""
 
-
 import argparse
 import os
 import sys
@@ -32,7 +31,7 @@ def get_args():
                         metavar='keyword',
                         type=str,
                         required=True,
-                        nargs=+,
+                        nargs='+',
                         help='Keyword to take')
 
     parser.add_argument('-s',
@@ -57,33 +56,33 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    keyword= args.keyword.lower()
-    skip_taxa = set(map(str.lower,args.skiptaxa or []))
+    keyword = args.keyword.lower()
+    skip_taxa = set(map(str.lower, args.skiptaxa or []))
     num_skipped = 0
     num_taken = 0
 
-    for rec in SeqI0.parse(args.file, 'swiss')
+    for rec in SeqI0.parse(args.file, 'swiss'):
         annot = rec.annotations
 
         if skip_taxa and 'taxonomy' in annot:
-            taxa = set (map(str.lower,taxa),annot['taxonomy'])
+            taxa = set(map(str.lower, taxa), annot['taxonomy'])
             if skip_taxa.intersection(taxa):
-            num_skipped += 1
+                num_skipped += 1
             continue
 
         if 'keywords' in annot:
-        kw = set(map(str.lower,annot['keywords']))
+            kw = set(map(str.lower, annot['keywords']))
 
         if keyword in kw:
             num_taken += 1
-            SeqIO.write(rec, args.outfile,'fasta')
+            SeqIO.write(rec, args.outfile, 'fasta')
         else:
-            num_skipped +=1
+            num_skipped += 1
 
         break
 
-    print (f'Done, skipped {num_skipped} and took {num_taken}.'
-        f'Sees output in "{args.outfile.name}".')
+    print(f'Done, skipped {num_skipped} and took {num_taken}.'
+          f'Sees output in "{args.outfile.name}".')
 
 
 # --------------------------------------------------
